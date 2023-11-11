3387e4bd104ebab46e22d22092caeae609e9e310
Lukas Rieder <1394536+lephyrus@users.noreply.github.com>
2023-11-11 22:55:54 +0100

    Use firstboot mechanics to install homebrew

--- config/files/usr/share/ublue-os/firstboot/launcher/autostart.sh
+++ config/files/usr/share/ublue-os/firstboot/launcher/autostart.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 # Simply launches the "yafti" GUI with the uBlue image's configuration.
-/usr/bin/yafti /usr/share/ublue-os/firstboot/yafti.yml
+#/usr/bin/yafti /usr/share/ublue-os/firstboot/yafti.yml
+
+# install homebrew and default brews
+/usr/share/ublue-os/firstboot/launcher/install-homebrew.sh
